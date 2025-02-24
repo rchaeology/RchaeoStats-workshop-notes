98dba6317d022b95adec52dd0e6c4fffd7b72449
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-02-24 15:43:06 +0100

    gitautosync update 5

--- RchaeoStats-workshop/scripts/01_data-viz.R
+++ RchaeoStats-workshop/scripts/01_data-viz.R
@@ -28,10 +28,48 @@ mortuary_data |>
   ggplot(aes(x = Phase, colour = Condition)) +
     geom_bar()
   
+# reorder chronologically  
+  
+mortuary_data |>
+  mutate(Phase = factor(Phase, levels = c("chi", "pre", "euro", "post", "disturbed"))) |>
+  ggplot(aes(x = Phase, fill = Phase)) +
+  geom_bar()  
+
+# convert explicitly to factor: option 1
 mortuary_data |>
   mutate(Condition = as_factor(Condition)) |>
-  ggplot(aes(x = Phase, colour = Condition)) +
-    geom_bar()
-  
-  
-  
+  ggplot(aes(x = Phase, fill = Condition)) +
+  geom_bar()
+# option 2
+mortuary_data |>
+  ggplot(aes(x = Phase, fill = as_factor(Condition))) +
+  geom_bar()
+
+# side-by-side bar plot (separate column for each level of Condition)
+mortuary_data |>
+  ggplot(aes(x = Phase, fill = as_factor(Condition))) +
+    geom_bar(position = "dodge")
+
+# proportional bar plot
+mortuary_data |>
+  ggplot(aes(x = Phase, fill = as_factor(Condition))) +
+  geom_bar(position = "fill")
+
+# Pie chart
+mortuary_data |>
+  ggplot(aes(x = "", fill = Phase)) +
+    geom_bar(width = 1) +
+    coord_polar("y", start = 0) +
+    theme_void()
+
+
+
+
+
+
+
+
+
+
+
+
