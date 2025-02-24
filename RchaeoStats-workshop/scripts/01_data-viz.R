792edf6a20670c29e041c87cd12b086574047106
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-02-24 15:09:53 +0100

    gitautosync update 3

--- RchaeoStats-workshop/scripts/01_data-viz.R
+++ RchaeoStats-workshop/scripts/01_data-viz.R
@@ -2,3 +2,11 @@ library(tidyverse)
 library(here)
 
 mortuary_data <- read_csv(here("data/mortuary_clean.csv"))
+
+mortuary_data |>
+  ggplot(aes(x = Length, y = Width)) +
+    geom_point()
+
+mortuary_data |>
+  ggplot(aes(x = Phase)) +
+    geom_bar()
