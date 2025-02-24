cbb1fa8f481758b668989c00a070bba361f9131d
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-02-24 15:26:40 +0100

    gitautosync update 4

--- RchaeoStats-workshop/scripts/01_data-viz.R
+++ RchaeoStats-workshop/scripts/01_data-viz.R
@@ -10,3 +10,28 @@ mortuary_data |>
 mortuary_data |>
   ggplot(aes(x = Phase)) +
     geom_bar()
+
+# Factors
+levels(mortuary_data$Phase)
+phase_factor <- as_factor(mortuary_data$Phase)
+levels(phase_factor) # level names
+nlevels(phase_factor) # number of levels
+length(levels(phase_factor)) # same as nlevels()
+
+factor(
+  phase_factor,
+  levels = c("chi", "pre", "euro", "post", "disturbed"),
+  ordered = TRUE
+)
+
+mortuary_data |>
+  ggplot(aes(x = Phase, colour = Condition)) +
+    geom_bar()
+  
+mortuary_data |>
+  mutate(Condition = as_factor(Condition)) |>
+  ggplot(aes(x = Phase, colour = Condition)) +
+    geom_bar()
+  
+  
+  
