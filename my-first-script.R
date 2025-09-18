e7246e528c6ab6bb661245c59cb7b9d11eb15620
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-09-18 12:31:52 +0200

    gitautosync update 1

--- my-first-script.R
+++ my-first-script.R
@@ -9,3 +9,35 @@ ggplot(data = sheep_data, mapping = aes(x = GLl, y = Bd)) +
   scale_colour_viridis_d(end = 0.8) +
   theme_minimal() +
   labs(x = "Greatest lateral length (mm)", y = "Breadth of the distal end (mm)")
+
+# Data transformation
+
+# transforming rows
+filter(sheep_data, Site == "KSN") # filter all rows from the KSN site
+
+filter(sheep_data, Site == "KSN" | Site == "ABM") # filter all rows from the KSN site or ABM site
+
+filter(sheep_data, Site != "DOR")
+
+sheep_data$Site != "DOR"
+
+filtered_site <- filter(sheep_data, Site != "DOR")
+
+arrange(filtered_site, GLl) # ascending order
+
+arrange(filtered_site, desc(GLl)) # descending order
+
+# transform columns
+
+mutate(filtered_site, dataset = "Sheep Astragali")
+
+dim_index <- mutate(filtered_site, Dl_GLl = Dl / GLl * 100)
+
+mutate(filter(sheep_data, Site != "DOR"), DL_GLl = Dl / GLl * 100)
+
+dim_index <- sheep_data |>
+  filter(Site != "DOR") |>
+  mutate(Dl_GLl = Dl / GLl * 100) |>
+  select(specID, Site, GLl, Dl) # select specific columns to keep
+
+
